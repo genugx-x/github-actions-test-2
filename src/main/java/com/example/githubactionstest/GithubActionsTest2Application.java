7ec259e964e34af10ab0cb2535ded502d11360ed
genug <qparanoya@gmail.com>
2022-11-17 14:44:09 +0900

    init commit

---
+++ src/main/java/com/example/githubactionstest/GithubActionsTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionstest/GithubActionsTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
